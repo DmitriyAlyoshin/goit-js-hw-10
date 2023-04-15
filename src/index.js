1fd8ff18de051f02e603417cb654f77f1d2c11ab
Dmitriy Alyoshin <valerich1715@gmail.com>
2023-04-15 22:51:38 +0200

    Update js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
 import './css/styles.css';
-import { fetchCountries } from './JS/fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
-
+import { fetchCountries } from './JS/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+let input = document.querySelector('#search-box');
+let countryList = document.querySelector('.country-list');
+let countryInfo = document.querySelector('.country-info');
+
+input.addEventListener(
+  'input',
+  debounce(e => {
+    let trimmedValue = input.value.trim();
+    cleanHtml();
+
+    if (trimmedValue !== '') {
+      fetchCountries(trimmedValue).then(foundData => {
+        if (foundData.length > 10) {
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.',
+            { position: 'center-top' }
+          );
+        } else if (foundData.length === 0) {
+          Notiflix.Notify.failure('Oops, there is no country with that name', {
+            position: 'center-top',
+          });
+        } else if (foundData.length >= 2 && foundData.length <= 10) {
+          renderCountryList(foundData);
+        } else if (foundData.length === 1) {
+          renderOneCountry(foundData);
+        }
+      });
+    }
+  }, DEBOUNCE_DELAY)
+);
